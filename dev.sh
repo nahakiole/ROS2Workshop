4215308ce91ed6b07515efc8b2e3e85beaf77fed
Elian Isac <isacel@student.ethz.ch>
2025-10-22 14:49:20 +0200

    cleanup

--- dev.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-source /opt/ros/jazzy/setup.bash
-cd /workspaces/ros2_ws
-colcon build --symlink-install
-source install/setup.bash
-echo "Environment ready. Try: ros2 run my_py_pkg talker"
